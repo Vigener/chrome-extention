7425180ba7521db3a1603de5769f3f4b64973a87
Vigener <66507155+Vigener@users.noreply.github.com>
2023-06-26 02:22:02 +0900

    test.js の更新

--- 図書館API/API取得テスト/test.js
+++ 図書館API/API取得テスト/test.js
@@ -3,7 +3,7 @@ fetch("https://www.googleapis.com/books/v1/volumes?q=星の王子さま&printTyp
   .then(data => {
     console.log(data);
     const data0 = data.items[0];
-    // console.log(data0);//最初の要素を表示する
+    console.log(data0);//最初の要素を表示する
     console.log(data0.volumeInfo.industryIdentifiers[0]);
     const isbn10_data0 = data0.volumeInfo.industryIdentifiers[0].identifier;
     console.log(isbn10_data0);
